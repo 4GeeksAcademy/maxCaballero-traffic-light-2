8f9aa138642d0f783bc6897527cedceffbbeb023
Christian_Caballero <94983984+xtiancr78@users.noreply.github.com>
2024-08-22 21:51:21 +0000

    proyecto finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,22 +9,12 @@ import TrafficLight from "./TrafficLight";
 const Home = () => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
 
-					<h1>Luces del Semafo</h1>
-					<TrafficLight />
 
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>Luces del Semafo</h1>
+			<TrafficLight />
+
+
 		</div>
 	);
 };
